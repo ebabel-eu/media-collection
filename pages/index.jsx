6589218d686544846f058b73da5d8e5e641fbe03
Thomas Amar <hello@ebabel.eu>
2022-04-14 19:20:00 +0200

    Add form to update or insert media items.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,45 +1,92 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>
 }
 
-export default function HomePage() {
-  const mediaItems = [
-    {
-      format: 'book',
-      mainTitle: 'La balle du néant',
-      subTitle: 'Les futurs mystères de Paris',
-      standardGeneralLocation: 'Thomas bedroom',
-      standardDetailedLocation: 'Sci-fi shelf next to bed',
-      currentLocation: 'On the table in the living room',
-    }
-  ]
-
-  const [likes, setLikes] = useState(0)
-
-  function handleClick() {
-    setLikes(likes + 1)
+function upsert(array, element) { // (1)
+  const i = array.findIndex(_element => _element.id === element.id);
+  if (i > -1) {
+    array[i] = element; // (2)
+  }
+  else { array.push(element);
   }
 
+  return array;
+}
+
+export default function HomePage() {
+  const [mediaItems, setMediaItems] = useState([]);
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    const toUpsert = {
+      id: document.querySelector('#id').value || Date.now(),
+      format: document.querySelector('#format').value,
+      title: document.querySelector('#title').value,
+      location: document.querySelector('#location').value,
+    };
+    toUpsert.id = isNaN(toUpsert.id) ? undefined : Number(toUpsert.id);
+
+    const updatedMediaItems = upsert(mediaItems, toUpsert);
+
+    setMediaItems([...updatedMediaItems]);
+  }
+
+  const editMediaItem = useCallback(
+    (mediaItem) => () => {
+      document.querySelector('#id').value = mediaItem.id;
+      document.querySelector('#format').value = mediaItem.format;
+      document.querySelector('#title').value = mediaItem.title;
+      document.querySelector('#location').value = mediaItem.location;
+    },
+    [],
+  );
+  
   return (
     <div>
-      <Header title="📚 Books 💿 CDs 📀 DVDs 🎧 Audio 🎬 Video" />
+      <Header title="📚 Book 💿 CD 📀 DVD 🎧 Audio 🎬 Video" />
       <ul>
         {mediaItems.map(item => (
-          <li key={item}>
+          <li key={item.id}>
             {item.format === 'book' ? '📚' : ''}
             {item.format === 'cd' ? '💿' : ''}
             {item.format === 'dvd' ? '📀' : ''}
             {item.format === 'audio' ? '🎧' : ''}
             {item.format === 'video' ? '🎬' : ''}
-            {item.mainTitle}
-            {item.subTitle ? ` (${item.subTitle})` : '' }<br />
-            {item.currentLocation}
+            {item.title}<br />
+            {item.location}
+            <button onClick={editMediaItem(item)} >Edit</button>
           </li>
         ))}
       </ul>
 
-      <button onClick={handleClick}>Like ({likes})</button>
+      <form onSubmit={handleClick} action="/aws-endpoint" method="POST">
+        <fieldset>
+          <legend>New Media Item</legend>
+          <input type="hidden" id="id" />
+          <label>
+            Format
+            <select id="format">
+              <option>book</option>
+              <option>cd</option>
+              <option>dvd</option>
+              <option>audio</option>
+              <option>video</option>
+            </select>
+          </label>
+          <label>
+            Main title
+            <input type="text" id="title" />
+          </label>
+          <label>
+            Location
+            <textarea id="location"></textarea>
+          </label>
+        </fieldset>
+        <button onClick={handleClick}>💾 Save</button>
+      </form>
     </div>
   )
 }
